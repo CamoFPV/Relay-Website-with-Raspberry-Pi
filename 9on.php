2c5c0ef293ce503888cf2d006891a7f2a69ad378
CamoFPV <admin@camofpv.com>
2016-12-23 23:03:27 -0500

    Update 9on.php

--- 9on.php
+++ 9on.php
@@ -1,6 +1,6 @@
 <?php
-$output = shell_exec('sudo python 9on.py 2>&1');
+$output = shell_exec('sudo python ./py/9on.py 2>&1');
 echo "<pre>$output</pre>";
-@include("./index.html");
+@include("./index.php");
 exit;
 ?>
