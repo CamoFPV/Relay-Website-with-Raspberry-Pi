224771cc15e368c7f2ad3f5e21817fea86c8a10a
CamoFPV <admin@camofpv.com>
2016-12-23 23:00:31 -0500

    Update 3off.php

--- 3off.php
+++ 3off.php
@@ -1,6 +1,6 @@
 <?php
-$output = shell_exec('sudo python 3off.py 2>&1');
+$output = shell_exec('sudo python ./py/3off.py 2>&1');
 echo "<pre>$output</pre>";
-header("Location: ./index.html");
+header("Location: ./index.php");
 exit;
 ?>
