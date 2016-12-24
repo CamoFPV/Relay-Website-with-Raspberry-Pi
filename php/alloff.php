6d5f5308fd1a26dea8a9b3473647cdae316abe07
CamoFPV <admin@camofpv.com>
2016-12-24 00:17:25 -0500

    Update alloff.php

--- php/alloff.php
+++ php/alloff.php
@@ -1,6 +1,6 @@
 <?php
-$output = shell_exec('sudo python ./py/alloff.py 2>&1');
+$output = shell_exec('sudo python ../py/alloff.py 2>&1');
 echo "<pre>$output</pre>";
-header("Location: /index.php");
+header("Location: ../index.php");
 exit;
 ?>
