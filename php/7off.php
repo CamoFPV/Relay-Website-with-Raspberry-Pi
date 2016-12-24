0a86afa2ad68e4bf399540b237cd0b26108bda48
CamoFPV <admin@camofpv.com>
2016-12-24 00:16:21 -0500

    Update 7off.php

--- php/7off.php
+++ php/7off.php
@@ -1,6 +1,6 @@
 <?php
-$output = shell_exec('sudo python ./py/7off.py 2>&1');
+$output = shell_exec('sudo python ../py/7off.py 2>&1');
 echo "<pre>$output</pre>";
-header("Location: /index.php");
+header("Location: ../index.php");
 exit;
 ?>
