a6063b2d06c2415f9d4385f40b90f4ba6a57b216
CamoFPV <admin@camofpv.com>
2016-12-23 23:00:43 -0500

    Update 3on.php

--- 3on.php
+++ 3on.php
@@ -1,6 +1,6 @@
 <?php
-$output = shell_exec('sudo python 3on.py 2>&1');
+$output = shell_exec('sudo python ./py/3on.py 2>&1');
 echo "<pre>$output</pre>";
-header("Location: ./index.html");
+header("Location: ./index.php");
 exit;
 ?>
