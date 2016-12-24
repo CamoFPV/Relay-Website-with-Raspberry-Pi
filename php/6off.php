ffcfa7f397854b4b5c19401f83e478c3de68ef30
CamoFPV <admin@camofpv.com>
2016-12-24 00:16:01 -0500

    Update 6off.php

--- php/6off.php
+++ php/6off.php
@@ -1,6 +1,6 @@
 <?php
-$output = shell_exec('sudo python ./py/6off.py 2>&1');
+$output = shell_exec('sudo python ../py/6off.py 2>&1');
 echo "<pre>$output</pre>";
-header("Location: /index.php");
+header("Location: ../index.php");
 exit;
 ?>
